4b65713e4fea1581e1f5457caf5f7a4b51c7daab
Prakruthi <prakruthiprasadk@gmail.com>
2024-10-07 12:26:17 +0530

    New addition to about me and weather data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (lat, long, callback) => {
             callback('Unable to find location!Try another search')
         }
         else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. But it feels like ${body.current.feelslike} degrees out.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}%`)
         }
     })
 }
